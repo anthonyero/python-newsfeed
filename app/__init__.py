a88946791a668928b9a92252d4c0080194d022eb
anthonyero <anthonyero@gmail.com>
2025-03-04 22:10:51 -0600

    In `app/__init__.py`, updated the `create_app()` function to register the `home` blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from app.routes import home
 from flask import Flask
 
 def create_app(test_config=None):
-  # set up app config
+  # Set up app config
   app = Flask(__name__, static_url_path='/')
   app.url_map.strict_slashes = False
   app.config.from_mapping(
@@ -12,5 +12,8 @@ def create_app(test_config=None):
   @app.route('/hello')
   def hello():
     return 'hello world'
+
+  # Register routes
+  app.register_blueprint(home)
 
   return app
