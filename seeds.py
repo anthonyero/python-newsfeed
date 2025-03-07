ad0c8c6cb009aa53ee3a8ef93eda526237d2a9dc
anthonyero <anthonyero@gmail.com>
2025-03-06 21:48:14 -0600

    In `seeds.py` established a temporary connection to the database using `Session()`, defined `User` data, committed/INSERTED our data to the "users" table, and closed the connection

--- seeds.py
+++ seeds.py
@@ -4,3 +4,18 @@ from app.db import Session, Base, engine # `engine` is the connection variable
 # Drop and rebuild tables
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
+
+db = Session() # Establishes a temporary session connection with the `Session` class
+
+# Insert users
+db.add_all([
+  User(username='alesmonde0', email='nwestnedge0@cbc.ca', password='password123'),
+  User(username='jwilloughway1', email='rmebes1@sogou.com', password='password123'),
+  User(username='iboddam2', email='cstoneman2@last.fm', password='password123'),
+  User(username='dstanmer3', email='ihellier3@goo.ne.jp', password='password123'),
+  User(username='djiri4', email='gmidgley4@weather.com', password='password123')
+])
+
+db.commit() # Necessary to run the `INSERT` statements 
+
+db.close() # Closes the connection
