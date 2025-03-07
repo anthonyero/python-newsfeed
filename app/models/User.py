7d4c9f85b51b11181fae6aa067ada2c39189bd6c
anthonyero <anthonyero@gmail.com>
2025-03-06 22:12:44 -0600

    In `User.py`, imported the `validates()` function from the `sqlalchemy` module. Added validation to the email field to ensure that the data contains `@`

--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,6 @@
 from app.db import Base # Import the Base class
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
 
 class User(Base): # Here we are creating a User class that inherits from the Base class
   __tablename__ = 'users'
@@ -7,3 +8,10 @@ class User(Base): # Here we are creating a User class that inherits from the Bas
   username = Column(String(50), nullable=False)
   email = Column(String(50), nullable=False, unique=True)
   password = Column(String(100), nullable=False)
+
+  @validates('email')
+  def validate_email(self, key, email):
+    # Make sure email address contains @ character
+    assert '@' in email # `assert` prompts a check. Will automaticallythrow an error if the condition is false, preventing the `return` statement from being called
+
+    return email
