129f1eed3c327f047f262fe3bcf8659899558b79
anthonyero <anthonyero@gmail.com>
2025-03-17 21:23:16 -0500

    In `app/models/Post.py`, defined a new dynamic relationship to return comments associate with a post when queried. Also, defined a cascade to delete all comments associated with a post if that post is deleted

--- app/models/Post.py
+++ app/models/Post.py
@@ -11,4 +11,6 @@ class Post(Base):
   user_id = Column(Integer, ForeignKey('users.id'))
   created_at = Column(DateTime, default=datetime.now)
   updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+  
   user = relationship('User')
+  comments = relationship('Comment', cascade='all,delete') # Returns comments associated with post. If a post is deleted, delete all of its associated comments as well
