e4ec60d147ac93921d5f33f2d048a226eb0f7c39
anthonyero <anthonyero@gmail.com>
2025-03-31 20:43:39 -0500

    In , removed the tests we used to validate the functions and provided comments to explain the purpose of each function and expected returns

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -1,23 +1,14 @@
+# Returns a date object in the following format "mm/dd/yy"
 def format_date(date):
   return date.strftime('%m/%d/%y')
 
-# This is to test the `format_date(date)` function. Simply run the file to validate 
-from datetime import datetime
-print(format_date(datetime.now()))
-
+# Returns a URL with only the domain name
 def format_url(url):
   return url.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0].split('?')[0]
 
-# This is to test the `format_url(url)` function
-print(format_url('http://google.com/test/'))
-print(format_url('https://www.google.com?q=test'))
-
+# Returns a pluralized version of the word depending on the amount provided
 def format_plural(amount, word):
   if amount != 1:
     return word + 's'
 
   return word
-
-# This is to test the `format_plural(amount, word)`
-print(format_plural(2, 'cat'))
-print(format_plural(1, 'dog'))
