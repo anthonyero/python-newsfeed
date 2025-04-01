fc7a32e13b3d686eef73d3f6f37cad3e8d34ee21
anthonyero <anthonyero@gmail.com>
2025-03-31 20:34:55 -0500

    In `app/utils/filters.py`, defined a `format_url(url)` function that removes extraneous information from a URL string and returns only the domain name

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -4,3 +4,10 @@ def format_date(date):
 # This is to test the `format_date(date)` function. Simply run the file to validate 
 from datetime import datetime
 print(format_date(datetime.now()))
+
+def format_url(url):
+  return url.replace('http://', '').replace('https://', '').replace('www.', '').split('/')[0].split('?')[0]
+
+# This is to test the `format_url(url)` function
+print(format_url('http://google.com/test/'))
+print(format_url('https://www.google.com?q=test'))
