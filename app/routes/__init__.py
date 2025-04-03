b57243e2cc328f619544cb19043e6018d5ea8e8b
anthonyero <anthonyero@gmail.com>
2025-04-02 21:34:38 -0500

    In `app/routes/__init__.py`, imported `bp` from `.api` and registered it as `api`

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,2 +1,3 @@
 from .home import bp as home # Import the Blueprint object, which contains the routes within it, and rename it `home`
 from .dashboard import bp as dashboard
+from .api import bp as api # Registers the API blueprint
