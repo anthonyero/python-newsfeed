1f17ad4898e3db67896f4ba88a9446bcf65fa346
anthonyero <anthonyero@gmail.com>
2025-04-07 21:59:19 -0500

    In `app/routes/api.py`, add `try...except` error handling to the signup route that returns a 500 error code if the creation of a new user failed

--- app/routes/api.py
+++ app/routes/api.py
@@ -10,15 +10,19 @@ def signup():
   data = request.get_json() # Retrieves the information from the form in a JSON object
   db = get_db()
   
-  # Create a new user
-  newUser = User( # Use bracket notation to access the properties in Python. Difference between dictionaries and objects 
-    username = data['username'],
-    email = data['email'],
-    password = data['password']
-  )
+  try: 
+    # Attempt to create a new user
+    newUser = User( # Use bracket notation to access the properties in Python. Difference between dictionaries and objects 
+      username = data['username'],
+      email = data['email'],
+      password = data['password']
+    )
 
-  # Save in database
-  db.add(newUser) # Preps the `INSERT` statement
-  db.commit() # Officially updates the database
+    # Save in database
+    db.add(newUser) # Preps the `INSERT` statement
+    db.commit() # Officially updates the database
+  except:
+    # Insert failed, so send error to the front end
+    return jsonify(message = 'Signup failed'), 500  
 
   return jsonify(id = newUser.id) # Returns a JSON object to the user/command line
