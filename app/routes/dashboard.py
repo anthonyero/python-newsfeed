71208b1a0a8598314f1f93207e85c78d578c1476
anthonyero <anthonyero@gmail.com>
2025-04-21 22:18:44 -0500

    In `app/routes/dashboard.py`, updated the `edit()` route to edit/delete an existing post.

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -21,4 +21,13 @@ def dash():
 
 @bp.route('/edit/<id>')
 def edit(id):
-  return render_template('edit-post.html')
+  # Get single post by id
+  db = get_db()
+  post = db.query(Post).filter(Post.id == id).one()
+
+  # Render edit page
+  return render_template(
+    'edit-post.html',
+    post=post,
+    loggedIn=session.get('loggedIn')
+  )
