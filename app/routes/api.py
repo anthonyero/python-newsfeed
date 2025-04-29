892ffbafc1cffbc11d6f80332e051877a190937a
anthonyero <anthonyero@gmail.com>
2025-04-28 22:31:41 -0500

    In `app/routes/api.py`, imported the `login_required` decorator from `app.utils.auth` and applied it to the `create()`, `update(id)`, `delete(id)`, and `upvote()`, and `comment()` routes

--- app/routes/api.py
+++ app/routes/api.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify, session # Request is another glob
 from app.models import User, Post, Comment, Vote
 from app.db import get_db
 import sys
+from app.utils.auth import login_required
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -69,6 +70,7 @@ def login():
   return jsonify(id = user.id)
 
 @bp.route('/comments', methods=['POST'])
+@login_required
 def comment():
   data = request.get_json()
   db = get_db()
@@ -91,6 +93,7 @@ def comment():
   return jsonify(id = newComment.id)
 
 @bp.route('/posts/upvote', methods=['PUT'])
+@login_required
 def upvote():
   data = request.get_json()
   db = get_db()
@@ -113,6 +116,7 @@ def upvote():
   return '', 204
 
 @bp.route('/posts', methods=['POST'])
+@login_required
 def create():
   data = request.get_json()
   db = get_db()
@@ -136,6 +140,7 @@ def create():
   return jsonify(id = newPost.id)
 
 @bp.route('/posts/<id>', methods=['PUT'])
+@login_required
 def update(id):
   data = request.get_json()
   db = get_db()
@@ -154,6 +159,7 @@ def update(id):
   return '', 204
 
 @bp.route('/posts/<id>', methods=['DELETE'])
+@login_required
 def delete(id):
   db = get_db()
 
